ed339c6d8d3f794f72007136d30498b15c62f7fc
İbrahim Can Zeybek <ibrahimcanzeybek@Ibrahims-MacBook-Air.local>
2025-01-25 09:35:07 +0300

    project done

--- src/functions/removeItemToWatchlist.js
+++ src/functions/removeItemToWatchlist.js
@@ -2,7 +2,7 @@ import { toast } from "react-toastify";
 
 export const removeItemToWatchlist = (e, id, setIsCoinAdded) => {
   e.preventDefault();
-  if (window.confirm("Are you sure you want to remove this coin?")) {
+  if (window.confirm("Bu coini izleme listesinden çıkarmak istediğinize emin misiniz?")) {
     let watchlist = JSON.parse(localStorage.getItem("watchlist"));
     const newList = watchlist.filter((coin) => coin != id);
     setIsCoinAdded(false);
@@ -10,14 +10,14 @@ export const removeItemToWatchlist = (e, id, setIsCoinAdded) => {
     toast.success(
       `${
         id.substring(0, 1).toUpperCase() + id.substring(1)
-      } - has been removed!`
+      } - çıkarıldı!`
     );
     window.location.reload();
   } else {
     toast.error(
       `${
         id.substring(0, 1).toUpperCase() + id.substring(1)
-      } - could not be removed!`
+      } -çıkarılmadı!`
     );
     setIsCoinAdded(true);
   }
